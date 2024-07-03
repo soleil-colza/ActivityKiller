1acd4c588f974883ca3fa6d4c70876c6a2eb2694
soleil.colza <soleil.colza@icloud.com>
2024-07-03 15:16:49 +0900

    removed unnecessary components

--- app/src/main/java/com/example/activitykiller/MainActivity.kt
+++ app/src/main/java/com/example/activitykiller/MainActivity.kt
@@ -20,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ActivityKillerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ActivityKillerTheme {
-        Greeting("Android")
     }
 }
