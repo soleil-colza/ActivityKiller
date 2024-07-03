0d97862275f41fecb07206cca5377abf9b9c7d54
soleil.colza <soleil.colza@icloud.com>
2024-07-03 17:02:49 +0900

    replaced the ktlint gradle plugin with version catalog declaration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    alias(libs.plugins.ktlint)
 }
