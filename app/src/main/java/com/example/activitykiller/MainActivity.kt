e7e1d423f8faca86dcd8d183914ea1fd8670fae7
soleil.colza <soleil.colza@icloud.com>
2024-07-03 16:49:47 +0900

    setup the pj to display different msgs referring to the activity's lifecycle

--- app/src/main/java/com/example/activitykiller/MainActivity.kt
+++ app/src/main/java/com/example/activitykiller/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.activitykiller.ui.theme.ActivityKillerTheme
+import com.example.activitykiller.ui.viewmodel.MessageViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +20,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ActivityKillerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val viewModel: MessageViewModel = viewModel()
+                    val message = viewModel.message.observeAsState("Android") // デフォルト値を設定
 
+                    if (savedInstanceState != null) {
+                        viewModel.setMessage("RIP")
+                    }
+
+                    Greeting(message.value)
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(message: String) {
+    Text(text = message)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ActivityKillerTheme {
+        Greeting("Android")
     }
 }
